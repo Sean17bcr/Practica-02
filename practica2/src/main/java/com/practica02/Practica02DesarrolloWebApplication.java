0bec35720b5755fb89038a278a348a383d22e3ca
Sean17bcr <106638574+Sean17bcr@users.noreply.github.com>
2022-10-03 21:13:55 -0600

    parc02

---
+++ practica2/src/main/java/com/practica02/Practica02DesarrolloWebApplication.java
@@ -0,0 +1,13 @@
+package com.practica02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica02DesarrolloWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica02DesarrolloWebApplication.class, args);
+	}
+
+}
